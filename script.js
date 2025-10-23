4e8d1b9762e097d89cc2dd1017166541201fbeb1
DudaOlivera <mariaoliveira94689@gmail.com>
2025-10-23 15:02:09 -0300

    feat: add base styling and initial product data

---
+++ script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const products = [
+        {
+            id: 1,
+            name: 'Cappuccino Cremoso',
+            price: 12.00,
+            image: 'assets/cappuccino.jpg' 
+        },
+        {
+            id: 2,
+            name: 'Latte Macchiato Caramelo',
+            price: 14.50,
+            image: 'assets/latte_caramelo.jpg' 
+        },
+        {
+            id: 3,
+            name: 'Torta Red Velvet Fatia',
+            price: 18.00,
+            image: 'assets/red_velvet.jpg' 
+        },
+        {
+            id: 4,
+            name: 'Croissant Tradicional',
+            price: 9.00,
+            image: 'assets/croissant.jpg' 
+        },
+        {
+            id: 5,
+            name: 'Mocaccino Especial',
+            price: 15.00,
+            image: 'assets/mocaccino.jpg' 
+        },
+        {
+            id: 6,
+            name: 'Bolo de Cenoura com Cobertura',
+            price: 17.50,
+            image: 'assets/bolo_cenoura.jpg' 
+        },
+        {
+            id: 7,
+            name: 'Expresso Duplo Intenso',
+            price: 9.00,
+            image: 'assets/expresso_duplo.jpg' 
+        },
+        {
+            id: 8,
+            name: 'Pão de Queijo Gourmet',
+            price: 7.00,
+            image: 'assets/pao_queijo.jpg' 
+        },
+        {
+            id: 9,
+            name: 'Affogato Clássico',
+            price: 19.00,
+            image: 'assets/affogato.jpg' 
+        }
+    ];
+
+});   
